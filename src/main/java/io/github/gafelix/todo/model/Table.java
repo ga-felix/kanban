9a023f75e38fb1593cd9219e2e30eaa40aeee848
ga-felix <gafelix@alumni.usp.br>
2023-11-21 12:32:44 -0300

    test: add test for TableWriter service

--- src/main/java/io/github/gafelix/todo/model/Table.java
+++ src/main/java/io/github/gafelix/todo/model/Table.java
@@ -1,27 +1,28 @@
 package io.github.gafelix.todo.model;
 
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 
-import java.util.LinkedList;
 import java.util.List;
 
 
 @Getter
 @Builder
+@EqualsAndHashCode
 public class Table {
 
     @Id
     private String id;
-    private final List<Column> columns = new LinkedList<>();
+    private List<Column> columns;
 
     @Getter
     @Builder
     public static class Column {
 
         private String label;
-        private final List<Card> cards = new LinkedList<>();
+        private List<Card> cards;
 
     }
 
