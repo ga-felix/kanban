e621bb98ef3effab998fc4647cedd1d59707ba98
ga-felix <gafelix@alumni.usp.br>
2023-11-22 22:32:48 -0300

    feat: add array initialization on user dto mapping logic

--- src/main/java/io/github/gafelix/todo/service/ServiceDtoMapperImpl.java
+++ src/main/java/io/github/gafelix/todo/service/ServiceDtoMapperImpl.java
@@ -4,6 +4,8 @@ import io.github.gafelix.todo.model.User;
 import io.github.gafelix.todo.request.ServiceDto;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 
 @Service
 public class ServiceDtoMapperImpl implements ServiceDtoMapper {
@@ -12,6 +14,7 @@ public class ServiceDtoMapperImpl implements ServiceDtoMapper {
     public User toUser(ServiceDto request) {
         return User.builder()
                 .id(request.getUserId())
+                .knownTablesIds(new ArrayList<>())
                 .build();
     }
 
