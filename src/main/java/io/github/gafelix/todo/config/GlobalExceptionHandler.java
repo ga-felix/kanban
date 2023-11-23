deebe854351607680b59c2ed4c50f1c2d6527024
ga-felix <gafelix@alumni.usp.br>
2023-11-23 19:05:15 -0300

    feat(exception): add BindException handling

--- src/main/java/io/github/gafelix/todo/config/GlobalExceptionHandler.java
+++ src/main/java/io/github/gafelix/todo/config/GlobalExceptionHandler.java
@@ -5,14 +5,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConversionException;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.sql.Timestamp;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -21,19 +22,39 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(buildResponse(ex), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<Object> handleBindException(BindException ex) {
+        return new ResponseEntity<>(buildBindResponse(ex), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<Object> handleNotFoundExceptions(Exception ex) {
         return new ResponseEntity<>(buildResponse(ex), HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler({HttpMessageNotReadableException.class, EntityAlreadyExistsException.class, MethodArgumentNotValidException.class, HttpMessageConversionException.class})
+    @ExceptionHandler({EntityAlreadyExistsException.class})
     public ResponseEntity<Object> handleBadRequestExceptions(Exception ex) {
         return new ResponseEntity<>(buildResponse(ex), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler({HttpMessageNotReadableException.class, MethodArgumentNotValidException.class, HttpMessageConversionException.class})
+    public ResponseEntity<Object> handleBadRequestBindExceptions(BindException ex) {
+        return new ResponseEntity<>(buildBindResponse(ex), HttpStatus.BAD_REQUEST);
     }
 
     private Map<Object, Object> buildResponse(Exception exception) {
         var body = new HashMap<>();
         body.put("message", exception.getLocalizedMessage());
+        body.put("timestamp", new Timestamp(System.currentTimeMillis()).toString());
+        body.put("exception", exception.getClass().getSimpleName());
+        return body;
+    }
+    private Map<Object, Object> buildBindResponse(BindException exception) {
+        var body = new HashMap<>();
+        List<String> errors = new ArrayList<>();
+        for (FieldError error : exception.getBindingResult().getFieldErrors()) {
+            errors.add(error.getField() + ": " + error.getDefaultMessage());
+        }
+        body.put("message", errors);
         body.put("timestamp", new Timestamp(System.currentTimeMillis()).toString());
         body.put("exception", exception.getClass().getSimpleName());
         return body;
